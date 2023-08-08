6db26fc4fd7f0f9fd1671dea8163a0bc21579de5
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-08 22:53:37 +0200

    test

--- resources/[strin]/strin_tunning/config.lua
+++ resources/[strin]/strin_tunning/config.lua
@@ -16,6 +16,7 @@ TunningZones = {
         label = "Star Screamers",
         coords = vector3(731.74468994141, -1088.7290039063, 22.169044494629),
         society = "starscreamers",
+        society = "starscreamers",
         societyCut = 0.20,
     },
 }
