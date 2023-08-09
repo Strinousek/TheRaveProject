12b5edac82b8c3a549af166bb194004f47564114
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-09 11:56:03 +0200

    new(jobs/jail) Jail basics

---
+++ resources/[strin]/strin_jail/server/main.lua
@@ -0,0 +1,29 @@
+JailedPlayers = {}
+
+Citizen.CreateThread(function()
+    MySQL.query.await([[
+        CREATE IF NOT EXISTS `jail` (
+            `owner` VARCHAR(255) NOT NULL,
+            `reason` VARCHAR(255) NULL DEFAULT NULL,
+            `jailed_on` VARCHAR(255) NULL DEFAULT NULL,
+            `duration` INT(11) NOT NULL DEFAULT 0,
+
+            PRIMARY KEY (`owner`),
+            UNIQUE KEY (`owner`)
+        )
+    ]])
+end)
+
+AddEventHandler("esx:playerLoaded", function(playerId, xPlayer)
+    local owner = xPlayer.identifier..":"..xPlayer.get("char_id")
+    if(JailedPlayers[owner]) then
+        -- jail that mf
+        return
+    end
+    local jailData = MySQL.single.await("SELECT * FROM `jail` WHERE `owner` = ?", { owner })
+    if(not jailData or not next(jailData)) then
+        return
+    end
+    JailedPlayers[owner] = jailData
+    -- jail that mf
+end)
