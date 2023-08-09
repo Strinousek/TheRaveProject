561fe20d642ceb51d447624ec0ced1dfc9315ccd
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-09 12:16:26 +0200

    update(jobs/jail) create db table if not exists

--- resources/[strin]/strin_jail/fxmanifest.lua
+++ resources/[strin]/strin_jail/fxmanifest.lua
@@ -8,9 +8,10 @@ shared_scripts {
 }
 
 client_scripts {
-
+    "client/main.lua",
 }
 
 server_scripts {
-
+    "@oxmysql/lib/MySQL.lua",
+    "server/main.lua",
 }
--- resources/[strin]/strin_jail/server/main.lua
+++ resources/[strin]/strin_jail/server/main.lua
@@ -2,10 +2,10 @@ JailedPlayers = {}
 
 Citizen.CreateThread(function()
     MySQL.query.await([[
-        CREATE IF NOT EXISTS `jail` (
+        CREATE TABLE IF NOT EXISTS `jail` (
             `owner` VARCHAR(255) NOT NULL,
             `reason` VARCHAR(255) NULL DEFAULT NULL,
-            `jailed_on` VARCHAR(255) NULL DEFAULT NULL,
+            `jailed_on` BIGINT(24) NULL DEFAULT NULL,
             `duration` INT(11) NOT NULL DEFAULT 0,
 
             PRIMARY KEY (`owner`),
