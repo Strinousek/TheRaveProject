31ac948b4ed3cd393cda10f419451cf0c2b00b40
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-14 00:59:32 +0200

    last pred testingem <3

--- resources/[strin]/strin_licenses/server/main.lua
+++ resources/[strin]/strin_licenses/server/main.lua
@@ -4,6 +4,130 @@ AddEventHandler("strin_characters:characterDeleted", function(identifier, charac
     })
 end)
 
+function GetLicenseTypes()
+    local licenseTypes = {}
+    for k,v in pairs(LICENSES) do
+        table.insert(licenseTypes, k)
+    end
+    return licenseTypes
+end
+
+ESX.RegisterCommand("givelicense", "user", function(xPlayer, args)
+    if(not args.citizenId or not args.licenseType or not LICENSES[args.licenseType]) then
+        return
+    end
+    local _source = xPlayer?.source
+    if(not _source) then
+        xPlayer = {
+            showNotification = function(message) print(message) end,
+            getJob = function() return { name = "police", grade_name = "boss" } end,
+            getGroup = function() return "admin" end,
+        }
+    end
+    local citizenId = ESX.SanitizeString(args.citizenId)
+    local job = xPlayer.getJob()
+    if((not lib.table.contains(exports.strin_jobs:GetLawEnforcementJobs(), job.name) or job.grade_name ~= "boss") and xPlayer.getGroup() ~= "admin") then
+        xPlayer.showNotification("Neoprávněná akce!", { type = "error" })
+        return
+    end
+    local identifier, characterId
+    if(citizenId:len() < 20) then
+        local result = MySQL.single.await("SELECT `identifier`, `char_id` FROM `characters` WHERE `char_identifier` = ?", {
+            citizenId
+        })
+        if(result and next(result)) then
+            identifier = result.identifier
+            characterId = result.char_id
+        end
+    else
+        local separatorIndex = citizenId:find(":")
+        if(separatorIndex) then
+            identifier = citizenId:sub(1, separatorIndex - 1)
+            characterId = citizenId:sub(1, separatorIndex + 1)
+        end
+    end
+    if(not identifier or not characterId) then
+        xPlayer.showNotification("Nezdařilo se najít postavu!", { type = "error" })
+        return
+    end
+    local hasLicense = CheckLicense(identifier, args.licenseType, characterId)
+    if(hasLicense) then
+        xPlayer.showNotification("Tato postava již tuto licenci má!", { type = "error" })
+        return
+    end
+
+    MySQL.insert("INSERT INTO `user_licenses` SET `owner` = ?, `type` = ?", {
+        identifier..":"..characterId,
+        args.licenseType
+    }, function()
+        xPlayer.showNotification("Postavě byla přidělena licence - "..args.licenseType)
+    end)
+end, true, {
+    help = "Dát licenci postavě",
+    arguments = {
+        { name = "citizenId", help = "CitizenID | identifier:char_id ", type = "string" },
+        { name = "licenseType", help = table.concat(GetLicenseTypes(), " | "), type = "string" }
+    }
+})
+
+ESX.RegisterCommand("removelicense", "user", function(xPlayer, args)
+    if(not args.citizenId or not args.licenseType or not LICENSES[args.licenseType]) then
+        return
+    end
+    local _source = xPlayer?.source
+    if(not _source) then
+        xPlayer = {
+            showNotification = function(message) print(message) end,
+            getJob = function() return { name = "police", grade_name = "boss" } end,
+            getGroup = function() return "admin" end,
+        }
+    end
+    local citizenId = ESX.SanitizeString(args.citizenId)
+    local job = xPlayer.getJob()
+    if((not lib.table.contains(exports.strin_jobs:GetLawEnforcementJobs(), job.name) or job.grade_name ~= "boss") and xPlayer.getGroup() ~= "admin") then
+        xPlayer.showNotification("Neoprávněná akce!", { type = "error" })
+        return
+    end
+    local identifier, characterId
+    if(citizenId:len() < 20) then
+        local result = MySQL.single.await("SELECT `identifier`, `char_id` FROM `characters` WHERE `char_identifier` = ?", {
+            citizenId
+        })
+        if(result and next(result)) then
+            identifier = result.identifier
+            characterId = result.char_id
+        end
+    else
+        local separatorIndex = citizenId:find(":")
+        if(separatorIndex) then
+            identifier = citizenId:sub(1, separatorIndex - 1)
+            characterId = citizenId:sub(1, separatorIndex + 1)
+        end
+    end
+    if(not identifier or not characterId) then
+        xPlayer.showNotification("Nezdařilo se najít postavu!", { type = "error" })
+        return
+    end
+    local hasLicense = CheckLicense(identifier, args.licenseType, characterId)
+    if(not hasLicense) then
+        xPlayer.showNotification("Tato postava tuto licenci nemá!", { type = "error" })
+        return
+    end
+
+    MySQL.query("DELETE FROM `user_licenses` WHERE `owner` = ? AND `type` = ?", {
+        identifier..":"..characterId,
+        args.licenseType
+    }, function()
+        xPlayer.showNotification("Postavě byla odendána licence - "..args.licenseType)
+    end)
+end, true, {
+    help = "Odebrat licenci postavě",
+    arguments = {
+        { name = "citizenId", help = "CitizenID | identifier:char_id ", type = "string" },
+        { name = "licenseType", help = table.concat(GetLicenseTypes(), " | "), type = "string" }
+    }
+})
+
 function AddLicense(identifier, licenseType, characterId)
     if(not characterId) then
        characterId = MySQL.scalar.await("SELECT `char_id` FROM `users` WHERE `identifier` = ?", { identifier })
