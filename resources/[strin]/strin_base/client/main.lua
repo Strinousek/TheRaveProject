aa9b6bac4d7503d5bddb317fe34b474fee2b7b05
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-16 01:40:40 +0200

    feat(base) regulate population density

--- resources/[strin]/strin_base/client/main.lua
+++ resources/[strin]/strin_base/client/main.lua
@@ -56,11 +56,20 @@ AddEventHandler("esx:enteredVehicle", function(plate)
     end
 end)
 
+
+local DENSITY_MULTIPLIER = 0.5
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		--DisableControlAction(0, 26, true)
 		DisableControlAction(0, 36, true)
+        
+	    SetVehicleDensityMultiplierThisFrame(DENSITY_MULTIPLIER)
+	    SetPedDensityMultiplierThisFrame(DENSITY_MULTIPLIER)
+	    SetRandomVehicleDensityMultiplierThisFrame(DENSITY_MULTIPLIER)
+	    SetParkedVehicleDensityMultiplierThisFrame(DENSITY_MULTIPLIER)
+	    SetScenarioPedDensityMultiplierThisFrame(DENSITY_MULTIPLIER, DENSITY_MULTIPLIER)
 	end
 end)
 
