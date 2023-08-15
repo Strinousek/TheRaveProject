c427b4b988b77866f9616ac8c20ce0c1731e70aa
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-16 01:10:02 +0200

    fix(axon) wrong display condition

--- resources/[strin]/strin_axon/client/main.lua
+++ resources/[strin]/strin_axon/client/main.lua
@@ -1,22 +1,19 @@
 local showAxon = false
-local checked = false
 
 RegisterNetEvent("esx:playerLoaded", function()
-    if(not checked) then
-        checked = true
-        if(ESX?.PlayerData?.job?.name == "police" and not showAxon) then
-            local code = lib.callback.await("strin_axon:getCode")
-            showAxon = true
-            SendNUIMessage({
-                display = true,
-                code = code,
-            })
-        end
+    if(ESX?.PlayerData?.job?.name == "police") then
+        local code = lib.callback.await("strin_axon:getCode")
+        showAxon = true
+        SendNUIMessage({
+            display = true,
+            code = code,
+        })
     end
 end)
 
 RegisterNetEvent('esx:setJob', function(job)
     if(ESX?.PlayerData?.job?.name == "police") then
+        showAxon = true
         if(not showAxon) then
             local code = lib.callback.await("strin_axon:getCode")
             showAxon = true
@@ -60,16 +57,13 @@ end)
 
 AddEventHandler("onResourceStart", function(resourceName)
     if(GetCurrentResourceName() == resourceName) then
-        if(not checked) then
-            checked = true
-            if(ESX?.PlayerData?.job?.name == "police" and not showAxon) then
-                local code = lib.callback.await("strin_axon:getCode")
-                showAxon = true
-                SendNUIMessage({
-                    display = true,
-                    code = code,
-                })
-            end
+        if(ESX?.PlayerData?.job?.name == "police") then
+            local code = lib.callback.await("strin_axon:getCode")
+            showAxon = true
+            SendNUIMessage({
+                display = true,
+                code = code,
+            })
         end
     end
 end)
