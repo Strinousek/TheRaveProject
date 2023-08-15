d77c80c4d0378c191dc068830d358da996234831
Jakub Mrkáček <kubamrkacek@gmail.com>
2023-08-15 12:41:54 +0200

    fix(inventory) remove license insert - deprecated

--- resources/[base]/ox_inventory/modules/bridge/esx/server.lua
+++ resources/[base]/ox_inventory/modules/bridge/esx/server.lua
@@ -122,6 +122,6 @@ function server.isPlayerBoss(playerId)
 	return xPlayer.job.grade_name == 'boss'
 end
 
-MySQL.ready(function()
+/*MySQL.ready(function()
 	MySQL.insert('INSERT IGNORE INTO `licenses` (`type`, `label`) VALUES (?, ?)', { 'weapon', 'Weapon License'})
-end)
+end)*/
